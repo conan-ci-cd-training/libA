cbc71aaf451a9cf7bb89099c6c3f6bf8597bf077
czoido <mrgalleta@gmail.com>
2020-03-17 13:45:42 +0100

    more comments

--- src/libA.cpp
+++ src/libA.cpp
@@ -5,5 +5,6 @@
 
 void hello_libA(int indent, const std::string& msg) {
     // This is going to add a double indent
+    // because it indents 2*indent
     std::cout << std::string(2*indent, ' ') << "libA: " << msg << std::endl;
 }
